02e73f597e75a892364081bd77e8d72286fe89dc
RakibulAzadd <rakibul.azad.hstu@example.com>
2024-04-02 14:19:06 +0600

    f

---
+++ 03. STL/10. next Permutation/next_permutation.cpp
@@ -0,0 +1,19 @@
+ #include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+   vector<int> v= { 1, 2, 3 };
+ 
+    sort(v.begin(), v.end());
+ 
+    cout << "The 3! possible permutations with 3 elements:\n";
+    do {
+        cout << v[0] << " " << v[1] << " " << v[2] << "\n";
+    } while (next_permutation(v.begin(), v.end()));
+ 
+    cout << "After loop: " <<v[0] << ' '
+         << v[1] << ' ' << v[2] << '\n';
+ 
+    return 0;
+}
