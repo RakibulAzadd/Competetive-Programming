a345750dd3a8b9c2c7d492214a075c5bdd093071
RakibulAzadd <rakibul.azad.hstu@example.com>
2024-04-23 11:29:45 +0600

    f

---
+++ 08. Dynamic Programming/06. 0-1 Knapsack/code.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 2e5+10;
+long long wt[105], val[105];
+long long dp[105][100005];
+
+ long long func(long long ind , long long wt_left){
+     if(wt_left== 0 ) return 0;
+     if(ind<0) return 0;
+     if(dp[ind][wt_left] !=-1) return dp[ind][wt_left];
+     long long  ans= func(ind-1,wt_left);
+     if(wt_left-wt[ind]>=0)
+     ans= max(ans, func(ind-1, wt_left-wt[ind]) + val[ind]);
+     return dp[ind][wt_left] = ans;
+ }
+
+ int main(){
+    memset(dp,-1,sizeof(dp));
+    int n, w;
+    cin>> n>> w;
+    for(int i=0;i<n;i++){
+        cin>> wt[i] >> val[i];
+    }
+    cout<< func(n-1,w)<<endl; 
+ }
